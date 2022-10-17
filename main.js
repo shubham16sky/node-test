0bf9055e713270aad4a30a656bad15fcc964e418
shubham16sky <shubhampc16@gmail.com>
2022-10-17 16:55:44 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var http = require('http');
+// var port = process.env.node_port;
+
+http.createServer(function (request , response){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+
+    response.end('Hello world\n');
+}).listen(process.env.nodePort)
+
+console.log('Server Running at port '+process.env.nodePort);
+
+
+
+
